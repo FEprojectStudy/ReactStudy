8932e7b6f2ffa889e0358d84348b98d813b45b4f
hiyoo95 <hiyoo95@gmail.com>
2021-10-10 02:11:18 +0900

    reset btn added

--- 알렉스데브/speedtest_class/src/speedtest.jsx
+++ 알렉스데브/speedtest_class/src/speedtest.jsx
@@ -49,13 +49,21 @@ class SpeedTest extends Component {
 
   renderAverage = () => {
     return this.state.result.length === 0 ? null : (
-      <div>
-        평균 시간:
-        {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms
-      </div>
+      <>
+        <div>
+          평균 시간:
+          {this.state.result.reduce((a, c) => a + c) / this.state.result.length}
+          ms
+        </div>
+        <button onClick={this.onReset}>Reset Result</button>
+      </>
     );
   };
-
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
   render() {
     const { state, message } = this.state;
     return (
